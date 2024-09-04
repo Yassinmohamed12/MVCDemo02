37aa174322b9fdf64cac16d0bfa591c54083c638
Yassinmohamed12 <114882832+Yassinmohamed12@users.noreply.github.com>
2024-09-04 16:46:55 +0300

    Delete Company.WebV3 directory

--- Company.WebV3/Company.WebV3/Models/ErrorViewModel.cs
+++
@@ -1,9 +0,0 @@
-namespace Company.WebV3.Models
-{
-    public class ErrorViewModel
-    {
-        public string? RequestId { get; set; }
-
-        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
-    }
-}
